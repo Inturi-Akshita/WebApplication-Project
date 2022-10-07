7dfc88b10cfb6c573f4c18ffa9f2564d3ab0d468
Inturi-Akshita <inturiakshita@gmail.com>
2022-10-07 16:25:29 +0530

    storing data in backend

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
+var serviceAccount = require("./key.json");
+
+initializeApp ({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+
 app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
@@ -15,6 +26,27 @@ app.get('/welcome', (req, res) => {
 app.get('/welcome/login', (req, res) => {
   res.render("login")
 })
+
+app.get('/welcome/registersubmit', (req, res) => {
+  const firstname = req.query.firstname;
+  const lastname = req.query.lastname;
+  const email = req.query.email;
+  const phno = req.query.phno;
+  const username = req.query.username;
+  const password = req.query.password;
+  const repassword = req.query.repassword;
+
+  db.collection("users")
+    .add({
+      name: firstname + lastname,
+      username: username,
+      password: password,
+    })
+    .then(() => {
+      res.render("login")
+    });
+
+});
 
 app.get('/welcome/register', (req, res) => {
   res.render("register")
