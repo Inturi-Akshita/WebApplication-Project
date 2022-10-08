050c23b092c349d107e8c76e0b866d1b1903990c
Inturi-Akshita <inturiakshita@gmail.com>
2022-10-08 22:33:24 +0530

    query page created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const request = require("request");
 
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
@@ -19,11 +20,12 @@ app.get('/', (req, res) => {
     res.render("welcome")
 })
 
+const score = 0;
+
 app.get('/loginsubmit', (req, res) => {
   const name = req.query.name;
   const username = req.query.username;
   const password = req.query.password;
-
   db.collection("users")
     .where("name", "==", name)
     .where("username", "==", username)
@@ -31,13 +33,23 @@ app.get('/loginsubmit', (req, res) => {
     .get()
     .then((docs) => {
       if(docs.size > 0) {
-        res.render("mainpage");
+        var userData = [];
+        db.collection("users")
+          .get()
+          .then((docs) => {
+            docs.forEach((doc) => {
+              userData.push(doc.data())
+            })
+          })
+          .then(() => {
+              res.render("mp", {userData : userData});
+          })
+        
       } else {
         res.send("Login failed");
       }
     });
 });
-
 app.get('/login', (req, res) => {
   res.render("login")
 })
@@ -53,19 +65,47 @@ app.get('/registersubmit', (req, res) => {
 
   db.collection("users")
     .add({
-      name: firstname + lastname,
+      name: firstname + " " + lastname,
       username: username,
       password: password,
+      score: 0,
     })
     .then(() => {
       res.render("login")
     });
 
 });
-
 app.get('/register', (req, res) => {
   res.render("register")
-})
+});
+app.get('/questionsubmit', (req, res) => {
+  const name = req.query.name;
+  const que = req.query.question;
+  var userData = [];
+    db.collection("users")
+      .where("name", "==", name)
+      .then({
+        
+        .add({
+
+        })
+      })
+      // .add({
+      //   que : que,
+      //   score : score + 5,
+      // })
+      .get()
+      //.then((docs) => {
+        //docs.forEach((doc) => {
+          //userData.push(doc.data())
+        
+        //})
+      
+
+      .then(() => {
+        res.render("quesubmit", {userData : userData});
+      })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
